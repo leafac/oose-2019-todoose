9797a8c482f9d0a5b136fb5a7f1dc403307c67b5
Leandro Facchinetti <me@leafac.com>
2019-06-13 12:04:39 -0400

    Connect to database

--- src/main/java/com/jhuoose/todoose/Server.java
+++ src/main/java/com/jhuoose/todoose/Server.java
@@ -3,12 +3,17 @@ package com.jhuoose.todoose;
 import io.javalin.Javalin;
 import io.javalin.staticfiles.Location;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Javalin app = Javalin.create();
+        var connection = DriverManager.getConnection("jdbc:sqlite:todoose.db");
+        var statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS items (identifier INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT)");
         var items = new ArrayList<Item>();
         app.get("/items", ctx -> {
             ctx.json(items);
