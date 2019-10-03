573ef0a5a2878c85f23f630487966dcc614e1aa1
Leandro Facchinetti <git@leafac.com>
2019-10-03 04:18:23 -0400

    Code written in Lecture 5

--- src/test/java/com/jhuoose/todoose/models/ItemTests.java
+++ src/test/java/com/jhuoose/todoose/models/ItemTests.java
@@ -3,12 +3,17 @@ package com.jhuoose.todoose.models;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ItemTests {
     @Test
     void testGettersAndSetters() {
+        // TODO: Fix this compilation error. Similar to how you did it in Assignment 4.
         var item = new Item(5, "This is a test");
         assertEquals(5, item.getIdentifier());
         assertEquals("This is a test", item.getDescription());
+        assertTrue(item.isCompleted());
     }
+
+    // TODO: Write a test for when ‘.isCompleted()’ is false. Not just another assertion on the test above, but a different test altogether.
 }
